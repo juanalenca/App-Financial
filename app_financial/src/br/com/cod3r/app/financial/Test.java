6e197bc92c64f2f4af323a0c3162801a2bc73aa5
juanalenca <juanbarros.dev@gmail.com>
2024-08-28 22:12:10 -0300

    Módulos e Reflection.

--- app_financial/src/br/com/cod3r/app/financial/Test.java
+++ app_financial/src/br/com/cod3r/app/financial/Test.java
@@ -1,5 +1,7 @@
 package br.com.cod3r.app.financial;
 
+import java.lang.reflect.Field;
+
 import br.com.cod3r.app.calculation.Calculator;
 import br.com.cod3r.app.calculation.internal.ArithmericOperations;
 
@@ -13,6 +15,17 @@ public class Test {
 		ArithmericOperations ao = new ArithmericOperations();
 		System.out.println(ao.sum(4, 5, 6));
 		
+		
+		try {
+			Field fieldId = Calculator.class.getDeclaredFields()[0];
+			fieldId.setAccessible(true);
+			fieldId.set(calc, "def");
+			fieldId.setAccessible(false);
+			System.out.println(calc.getId());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+			
 	}
 
 }
