0f24d45a6aca04926f1374d818c8edaccfad766c
juanalenca <juanbarros.dev@gmail.com>
2024-09-10 21:24:20 -0300

    Utilizando Provides With e Uses

--- app_financial/src/br/com/cod3r/app/financial/Test.java
+++ app_financial/src/br/com/cod3r/app/financial/Test.java
@@ -1,29 +1,23 @@
 package br.com.cod3r.app.financial;
 
 import java.lang.reflect.Field;
+import java.util.ServiceLoader;
 
-import br.com.cod3r.app.calculation.Calculator;
-import br.com.cod3r.app.calculation.internal.ArithmericOperations;
+import br.com.cod3r.app.Calculator;
 
 public class Test {
 	
 	public static void main(String[] args) {
 		
-//		Calc c1 = new CalcImpl1();
-//		Calc c2 = new CalcImpl2();
-//		
-//		System.out.println(c1.sum(1, 2, 3));
-//		System.out.println(c2.sum(1, 2, 3));
-		
-		Calculator calc = new Calculator();
+		Calculator calc = ServiceLoader
+				.load(Calculator.class)
+				.findFirst()
+				.get();
 		System.out.println(calc.sum(2, 2, 3));
-		
-		ArithmericOperations ao = new ArithmericOperations();
-		System.out.println(ao.sum(4, 5, 6));
 		
 		
 		try {
-			Field fieldId = Calculator.class.getDeclaredFields()[0];
+			Field fieldId = calc.getClass().getDeclaredFields()[0];
 			fieldId.setAccessible(true);
 			fieldId.set(calc, "def");
 			fieldId.setAccessible(false);
--- app_financial/src/module-info.java
+++ app_financial/src/module-info.java
@@ -2,6 +2,8 @@ module app.financial {
 	
 	//requerendo os modolos que desejamos
 	requires java.base; //por padão
-	requires app.calculation;
+	//requires app.calculation;
+	requires app.api;
+	uses br.com.cod3r.app.Calculator;
 	
 }
